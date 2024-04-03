38597effee2f51d0d2726964a5e62bb90826a040
Monicapiya <piyalimonica@gmail.com>
2024-04-03 17:45:18 +0530

    prac

--- java-script/datatypes.js
+++ java-script/datatypes.js
@@ -12,3 +12,15 @@ const person = { firstName: "John", lastName: "Doe" };
 const cars = ["Saab", "Volvo", "BMW"];
 
 const date = new Date("2022-03-25");
+
+let isOnSale = true;
+console.log(isOnSale);
+isOnSale = false;
+console.log(isOnSale);
+
+// true answers
+// false
+
+let isTrue;
+isTrue = true;
+console.log(isTrue);
