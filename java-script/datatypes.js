7996993688b5a529557354c888b7a9f46a7162fd
Monicapiya <piyalimonica@gmail.com>
2024-04-03 16:50:57 +0530

    prac

---
+++ java-script/datatypes.js
@@ -0,0 +1,14 @@
+let length = 16;
+let weight = 7.5;
+
+let color = "Yellow";
+let lastName = "Johnson";
+
+let x = true;
+let y = false;
+
+const person = { firstName: "John", lastName: "Doe" };
+
+const cars = ["Saab", "Volvo", "BMW"];
+
+const date = new Date("2022-03-25");
