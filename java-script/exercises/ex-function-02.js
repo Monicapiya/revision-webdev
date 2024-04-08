8308e4c9ab3a152516a1eddcdd3aae07834c8642
Monicapiya <piyalimonica@gmail.com>
2024-04-08 14:35:58 +0530

    prac

---
+++ java-script/exercises/ex-function-02.js
@@ -0,0 +1,21 @@
+function getMilk(bottles) {
+  console.log("leave House");
+  console.log("Move Right ");
+  console.log("Move Right");
+  console.log(" Move Up");
+  console.log(" Move Up ");
+  console.log(" Move Up ");
+  console.log("Enter Store ");
+  console.log("Buy " + bottles + "  bottles of Milk ");
+  console.log("Exit Store ");
+  console.log("Move Down ");
+  console.log("Move Down  ");
+  console.log(" Move Down ");
+  console.log("Move Left ");
+  console.log(" Move Left ");
+  console.log("Enter House ");
+  
+
+}
+
+getMilk(2);
---
+++ java-script/exercises/ex-function-03.js
@@ -0,0 +1,24 @@
+function getMilk(money) {
+  console.log("leave House");
+  console.log("Move Right ");
+  console.log("Move Right");
+  console.log(" Move Up");
+  console.log(" Move Up ");
+  console.log(" Move Up ");
+  console.log("Enter Store ");
+
+  var numberofBottles = Math.floor(money / 1.5);
+
+  console.log("Buy " + numberofBottles + "  bottles of Milk ");
+  console.log("Exit Store ");
+  console.log("Move Down ");
+  console.log("Move Down  ");
+  console.log(" Move Down ");
+  console.log("Move Left ");
+  console.log(" Move Left ");
+  console.log("Enter House ");
+  
+
+}
+
+getMilk(5);
