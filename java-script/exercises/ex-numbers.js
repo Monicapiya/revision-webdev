e507befe8fd4d78a39f508b6709f44148b88cb0e
Monicapiya <piyalimonica@gmail.com>
2024-04-08 10:08:25 +0530

    prac

---
+++ java-script/exercises/ex-numbers.js
@@ -0,0 +1,4 @@
+// humanAge = (dogAge - 2) * 4 + 21
+var dogAge = prompt("What is your dog age?")
+var humanAge = (dogAge - 2) * 4 + 21;
+alert("Your dog age is " + humanAge + " old in human years");
