b079fda07769f524a1a7219722db753861399b45
Monicapiya <piyalimonica@gmail.com>
2024-04-08 10:39:55 +0530

    prac

---
+++ java-script/exercises/ex-function-01.js
@@ -0,0 +1,21 @@
+function getMilk() {
+  console.log("leave House");
+  console.log("Move Right ");
+  console.log("Move Right");
+  console.log(" Move Up");
+  console.log(" Move Up ");
+  console.log(" Move Up ");
+  console.log("Enter Store ");
+  console.log("Buy Milk ");
+  console.log("Exit Store ");
+  console.log("Move Down ");
+  console.log("Move Down  ");
+  console.log(" Move Down ");
+  console.log("Move Left ");
+  console.log(" Move Left ");
+  console.log("Enter House ");
+  
+
+}
+
+getMilk();
