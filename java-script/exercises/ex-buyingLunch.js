5f61d7da1f81a9d80ccdaa3c4da437715d4f568c
Monicapiya <piyalimonica@gmail.com>
2024-04-09 16:49:14 +0530

    preac

---
+++ java-script/exercises/ex-buyingLunch.js
@@ -0,0 +1,14 @@
+/*
+
+write a function which will select a random name from a list of names. 
+The person selected will have to pay for everybody's food bill.
+
+*/
+function whosPaying(names) {
+  var names = ["Angela", "Ben", "Jenny", "Michael", "Chloe"];
+  var numberOfPeople = names.length;
+  var randomPersonPosition = Math.floor(Math.random() * numberOfPeople);
+  var randomPerson = names[randomPersonPosition];
+
+  return randomPerson + " is going to buy lunch today!";
+}
