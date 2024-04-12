8e47f4bd047a7ea19e7f78c2a61f627c47b74bdf
Monicapiya <piyalimonica@gmail.com>
2024-04-12 07:36:20 +0530

    prac

--- Projects/Drum-Kit/index.js
+++ Projects/Drum-Kit/index.js
@@ -2,6 +2,8 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i<numberOfDrumButtons; i++)
 
-document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+
+function handleClick() {
   alert("clicked");
-});
+}
