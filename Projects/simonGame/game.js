f8938fdd397704f7667b1e6e85a84fd1eb456303
Monicapiya <piyalimonica@gmail.com>
2024-04-16 18:41:59 +0530

    prac

--- Projects/simonGame/game.js
+++ Projects/simonGame/game.js
@@ -1,6 +1,12 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
+var userClickedPattern = [];
+
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+});
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
@@ -12,6 +18,10 @@ function nextSequence() {
     .fadeOut(100)
     .fadeIn(100);
 
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
